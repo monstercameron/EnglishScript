fcce0dea2bcc476183176288cfc7c12dca110ef5
Earl Cameron <mr.e.cameron@gmail.com>
2023-06-22 19:16:50 -0400

    init commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import openai
+
+from dotenv import load_dotenv
+load_dotenv() 
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "user", "content": "Tell the world about the ChatGPT API in the style of a pirate."}
+  ]
+)
+
+print(completion.choices[0].message.content)
