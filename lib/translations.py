decbc4a7f8f6ad0a37e542f7a27fdb02937d0c01
Earl Cameron <mr.e.cameron@gmail.com>
2023-06-22 20:19:03 -0400

    init testing

---
+++ lib/translations.py
@@ -0,0 +1,16 @@
+(let value = 1;I have a fixed word value of 'string',) (const value = 'string';) (let value = 'string'; have a word value of 'string' ,) (const value = 'string';let carDoor = 5;I have a fixed word value of 'string' and a number carDoor of 5)"}
+
+pairs = [
+    {"javascript": "", "english": ""},
+    {"javascript": "const value = 1;", "english": "I have a fixed number value of 1"},
+    {"javascript": "let value = 1;", "english": "I have a fixed word value of 'string'"},
+    {"javascript": "", "english": ""},
+    {"javascript": "", "english": ""},
+]
+
+alias = [
+    ["const","constant","fixed"]
+]
+
+
+rules = {"role": "user", "content": ""}
